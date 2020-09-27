9bc55840ecb741feb3e6db4f9d8214df05a21562
matrix <786440445@qq.com>
2020-09-27 11:31:30 +0800

    'topk'

--- 二叉树/快速排序.py
+++ 二叉树/快速排序.py
@@ -26,41 +26,46 @@ def quickSort(arr, low, high):
         quickSort(arr, pi + 1, high)
 
 
-arr = [10, 15, 7, 8, 9, 1, 5]
-n = len(arr)
-quickSort(arr, 0, n - 1)
-print(arr)
+# arr = [10, 15, 7, 8, 9, 1, 5]
+# n = len(arr)
+# quickSort(arr, 0, n - 1)
+# print(arr)
 
 
-def quick_sort1(arr, low, high):
-    if low < high:
-        mid = partition1(arr, low, high)
-        quick_sort1(arr, low, mid-1)
-        quick_sort1(arr, mid+1, high)
+# 快排求topk
+def min_num(arr, m):
+    start, end = 0, len(arr) - 1
+    index = quick_index(arr, start, end)
+    while index != m:
+        if index < m:
+            index = quick_index(arr, index + 1, end)
+        else:
+            index = quick_index(arr, start, index)
+    return arr[:m]
 
 
-def partition1(arr, left, right):
+def quick_index(arr, left, right):
     low = left
-    high = right-1
-    pivot = arr[right]
+    high = right
+    mid = arr[left]
     while low < high:
-        while low < high and arr[low] <= pivot:
-            low += 1
-        while high > low and arr[high] > pivot:
+        while low < high and arr[high] <= mid:
             high -= 1
-        arr[low], arr[high] = arr[high], arr[low]
-
-    if arr[high] > pivot:
-        arr[high], arr[right] = arr[right], arr[high]
-        return high
-    else:
-        arr[high+1], arr[right] = arr[right], arr[high+1]
-        return high + 1
+        if low < high:
+            arr[low] = arr[high]
+            low += 1
+        while low < high and arr[low] >= mid:
+            low += 1
+        if low < high:
+            arr[high] = arr[low]
+            high -= 1
+    arr[low] = mid
+    return low
 
 
 arr = [5, 2, 1, 3, 8, 7, 6, 4]
-quick_sort1(arr, 0, len(arr)-1)
-print(arr)
+res = min_num(arr, 2)
+print(res)
 
 
 
